55fa26c5f5628e378ea714de60fa6d5a6b830b8b
mrs_misteriya <nata.nedostupnaya@mail.com>
2024-04-07 16:06:24 +0300

    adds task2

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,3 +1,10 @@
-export const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import styled from './Statistics.module.css'
+import { IconContext } from "react-icons";
+
+export const StatisticsItem = ({total, title, icon }) => {
+  return <li className={styled.item}>
+    <IconContext.Provider value={{ size:32}}>{icon}</IconContext.Provider>
+    <span className={styled.counter}>{ total}</span>
+    <p className={styled.text}>{ title}</p>
+  </li>;
 };
